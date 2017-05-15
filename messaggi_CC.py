4f9cb339edaa35b6c1192e5e33fcbee6d9accfe1
rmarzocchi84 <roberto.marzocchi@gter.it>
2017-05-15 18:26:47 +0200

    Rimossi delay e controllo sui tempi nel caso in cui si interrompano le comunicazioni per un po in modo da non generare allarmi vecchi e sbagliati e nemmeno incasinare i led con una marea di messaggi

--- messaggi_CC.py
+++ messaggi_CC.py
@@ -114,9 +114,6 @@ while True:
     #GPIO.output(4 , False)
     #GPIO.output(9 , False)
     #GPIO.output(22, False)
-
-
-
     #wait to accept a connection - blocking call
     #da testare se il server non manda dati come nitarlo accendendo comunque una luce...
     #print "fin qua ci arriva"
@@ -144,88 +141,102 @@ while True:
     #print conn    
     # calcolo ora UTC nello stesso formato dell'output di RTKLIB
     dt=datetime.utcnow()
+    ora_client=datetime.now()
     #sovrascrivo il time start per il check sul funzionamento degradato
     start = time.time()
     # Formatting datetime
-    ora=dt.strftime("%Y/%m/%d|%H:%M:%S.%f")
-    print ora
+    ora=ora_client.strftime("%Y/%m/%d %H:%M:%S.%f")
+    print "\nora=",ora
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
     #data = unicode(conn.recv(BUFFER_SIZE))
     data = conn.recv(BUFFER_SIZE)            
     dati=data.split('|')
     print data
-    stringa=dati[1]
-    print "dati0=",dati[0]
-    print stringa
-    i=0
-    allarme=0
-    GPIO.output(22 ,False)
-    GPIO.output(9 , False)
-    GPIO.output(4 , False)
-    #GPIO.output(11 , False)
-    GPIO.output(10 ,False)
-    GPIO.output(17 ,False)    
-    time.sleep(timeLed)
-    #cerco gli allarmi dell'algoritmo
-    while (i<int(dati[0])):
-        #print stringa[i]
-        if (int(stringa[i])>0):
-            #devo gestire gli allarmi
-            if (int(stringa[i])==2):
-                allarme=int(stringa[i])
-                break
-            else:                 
-                allarme=int(stringa[i])
-        i=i+1
-    print "allarme=",allarme
-    if allarme==0:
-        print "accendo il verde"
-        GPIO.output(8 , False)      
-        GPIO.output(4 , False)
-        GPIO.output(22 , False)
-        GPIO.output(9 , True)
-    elif (allarme==1):
-        print "accendo il giallo"
-        GPIO.output(4 , False)
-        GPIO.output(9 , False)
-        GPIO.output(22, True)
-        GPIO.output(8 , True)
-        time.sleep(timeLed/4)
-        GPIO.output(8 , False)
-    elif allarme==2:
-        print "accendo il ROSSO e il buzzer"
-        GPIO.output(22 , False)
-        GPIO.output(9, False)         
-        GPIO.output(8 , True)
-        GPIO.output(4 , True)
-    # cerco funzionamento degradato
-    if int(dati[2])==1:
-        GPIO.output(10 , True)
-    else:
-        GPIO.output(10 , False)
+    try:
+        stringa=dati[1]
+        check_stringa=0
+    except:
+        check_stringa=1
+    if check_stringa==0:    
+        print "dati0=",dati[0]
+        print stringa
+        ora_server=time.strptime(dati[5],"%Y%m%d %H:%M:%S")
+        ora_client1=time.strptime(ora,"%Y/%m/%d %H:%M:%S.%f")
+        #print "\nora server=", ora_server
+        #print "\nora client=", ora_client1
+        differenza=time.mktime(ora_client1)-time.mktime(ora_server)
+        print "*******************\ndifferenza=", differenza
+        print "\n*******************\n"
+        if differenza < 2:
+            i=0
+            allarme=0
+            GPIO.output(22 ,False)
+            GPIO.output(9 , False)
+            GPIO.output(4 , False)
+            #GPIO.output(11 , False)
+            GPIO.output(10 ,False)
+            GPIO.output(17 ,False)    
+            time.sleep(timeLed)
+            #cerco gli allarmi dell'algoritmo
+            while (i<int(dati[0])):
+                #print stringa[i]
+                if (int(stringa[i])>0):
+                    #devo gestire gli allarmi
+                    if (int(stringa[i])==2):
+                        allarme=int(stringa[i])
+                        break
+                    else:                 
+                        allarme=int(stringa[i])
+                i=i+1
+            print "allarme=",allarme
+            if allarme==0:
+                print "accendo il verde"
+                GPIO.output(8 , False)      
+                GPIO.output(4 , False)
+                GPIO.output(22 , False)
+                GPIO.output(9 , True)
+            elif (allarme==1):
+                print "accendo il giallo"
+                GPIO.output(4 , False)
+                GPIO.output(9 , False)
+                GPIO.output(22, True)
+                GPIO.output(8 , True)
+                time.sleep(timeLed/4)
+                GPIO.output(8 , False)
+            elif allarme==2:
+                print "accendo il ROSSO e il buzzer"
+                GPIO.output(22 , False)
+                GPIO.output(9, False)         
+                GPIO.output(8 , True)
+                GPIO.output(4 , True)
+            # cerco funzionamento degradato
+            if int(dati[2])==1:
+                GPIO.output(10 , True)
+            else:
+                GPIO.output(10 , False)
 
-    # cerco allarme generico
-    if int(dati[3])>0:
-        print "ALLARME GENERICO: accendo l'altro rosso e il buzzer"
-        GPIO.output(17 , True)
-        GPIO.output(8 , True)
-    else:        
-        if allarme>0 :
-            print "Controllo che il rosso dell'allarme generico sia spento."
-            GPIO.output(17 , False)
-        else :
-            print "Se non ci sono allarmi ne' generici ne' da CC spengo il buzzer"
-            GPIO.output(17 , False)
-            GPIO.output(8 , False)
+            # cerco allarme generico
+            if int(dati[3])>0:
+                print "ALLARME GENERICO: accendo l'altro rosso e il buzzer"
+                GPIO.output(17 , True)
+                GPIO.output(8 , True)
+            else:        
+                if allarme>0 :
+                    print "Controllo che il rosso dell'allarme generico sia spento."
+                    GPIO.output(17 , False)
+                else :
+                    print "Se non ci sono allarmi ne' generici ne' da CC spengo il buzzer"
+                    GPIO.output(17 , False)
+                    GPIO.output(8 , False)
 
-    # cerco near alarm
-    if (int(dati[4])>0 and allarme==0):
-        GPIO.output(17 , True)
-    else :
-        GPIO.output(17 , False)
-        
-    time.sleep(timeLed)
-    conn.send('OK\0')
+            # cerco near alarm
+            if (int(dati[4])>0 and allarme==0):
+                GPIO.output(17 , True)
+            else :
+                GPIO.output(17 , False)
+                
+            #time.sleep(timeLed)
+            conn.send('OK\0')
 
 s.close()
 
