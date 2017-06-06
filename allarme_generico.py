45de5d1d124b62c82b55898f742ae2e83b368006
rmarzocchi84 <roberto.marzocchi@gter.it>
2017-06-06 16:26:58 +0200

    corretta lettura della stringa con indirizzo IP in base al parsing del json in C

--- allarme_generico.py
+++ allarme_generico.py
@@ -69,7 +69,7 @@ for riga in file(nomefile1): # lettura file
 	i+=1
 
 id_apparato=parametri[0]
-TCP_IP=parametri[2]
+TCP_IP=parametri[2].replace("\\\"","")
 
 
 print "#######################################"
